76a809102414d47db5c76f694fb44685d22ec494
jjuun0 <jh061993@gmail.com>
2021-04-25 22:26:58 +0900

    Siamese Network made
    직접 샴 네트워크에 학습시키고 평가해봤다(얼굴 인식)

---
+++ Test.py
@@ -0,0 +1,54 @@
+import torchvision.datasets as dset
+from Dataset import SiameseNetworkDataset
+import torchvision.transforms as transforms
+from torch.utils.data import DataLoader
+import torch
+import matplotlib.pyplot as plt
+import numpy as np
+from torch.autograd import Variable
+import torch.nn.functional as F
+import torchvision.utils
+from Network import SiameseNetwork
+from Train import MODELNAME
+
+
+def imshow(img, text=None, should_save=False):
+    npimg = img.numpy()
+    plt.axis("off")
+    if text:
+        plt.text(75, 8, text, style='italic', fontweight='bold',
+                 bbox={'facecolor': 'white', 'alpha': 0.8, 'pad': 10})
+
+    # PyTorch 는 이미지 데이터셋을 [Batch Size, Channel, Width, Height] 순서대로 저장
+    # -> matplotlib 로 출력하기 위해서는 각 이미지를 [Width, Height, Channel] 형태로 변경
+    plt.imshow(np.transpose(npimg, (1, 2, 0)))
+    plt.show()
+
+
+testing_dir = "img/test/"
+
+
+def test():
+    net = SiameseNetwork()
+    net.load_state_dict(torch.load('./'+MODELNAME))
+    net.eval()
+    net.cuda()
+
+    test_dataset_folder = dset.ImageFolder(root=testing_dir)
+    siamese_test_dataset = SiameseNetworkDataset(dataPath=test_dataset_folder,
+                                            transform=transforms.Compose([transforms.Resize((100, 100)),
+                                                                          transforms.ToTensor()]),
+                                            should_invert=False)
+
+
+    test_dataloader = DataLoader(siamese_test_dataset, num_workers=0, batch_size=1, shuffle=True)
+    dataiter = iter(test_dataloader)
+    x0, _, _ = next(dataiter)
+
+    for i in range(20):
+        _, x1, label2 = next(dataiter)
+        concatenated = torch.cat((x0, x1), 0)
+
+        output1, output2 = net(Variable(x0).cuda(), Variable(x1).cuda())
+        euclidean_distance = F.pairwise_distance(output1, output2)
+        imshow(torchvision.utils.make_grid(concatenated), 'Dissimilarity: {:.2f}'.format(euclidean_distance.item()))
---
+++ Train.py
@@ -0,0 +1,56 @@
+import torchvision.datasets as dset
+from Dataset import SiameseNetworkDataset
+import torchvision.transforms as transforms
+from Network import SiameseNetwork
+from ContrastiveLoss import ContrastiveLoss
+from torch import optim
+from torch.utils.data import DataLoader
+import matplotlib.pyplot as plt
+import torch
+from torch.autograd import Variable
+
+
+
+MODELNAME = 'trained_model' + '.pt'
+
+training_dir = "img/train/"
+train_batch_size = 16
+train_number_epochs = 100
+
+
+def train():
+    train_dataset_folder = dset.ImageFolder(root=training_dir)
+
+    siamese_train_dataset = SiameseNetworkDataset(dataPath=train_dataset_folder,
+                                            transform=transforms.Compose([transforms.Resize((100, 100)), transforms.ToTensor()]),
+                                            should_invert=False)
+
+    train_dataloader = DataLoader(siamese_train_dataset, shuffle=True, num_workers=0, batch_size=train_batch_size)
+
+    net = SiameseNetwork().cuda()
+    criterion = ContrastiveLoss()
+    optimizer = optim.Adam(net.parameters(), lr=0.0005)
+
+    counter = []
+    loss_history = []
+    iteration_number = 0
+
+    for epoch in range(train_number_epochs):
+        for i, data in enumerate(train_dataloader, 0):
+            img0, img1, label = data
+            img0, img1, label = Variable(img0).cuda(), Variable(img1).cuda(), Variable(label).cuda()
+            output1,output2 = net(img0, img1)
+            optimizer.zero_grad()
+            loss_contrastive = criterion(output1, output2, label)
+            loss_contrastive.backward()
+            optimizer.step()
+            if i % 10 == 0:
+                print("Epoch number {}\n Current loss {}\n".format(epoch,loss_contrastive.item()))
+                iteration_number += 10
+                counter.append(iteration_number)
+                loss_history.append(loss_contrastive.item())
+
+    print('Training done.')
+    torch.save(net.state_dict(), "./"+MODELNAME)
+    plt.plot(counter, loss_history)
+    plt.show()
