2e0b4f5bf5ddd84aebe9293462e028d23a88fb69
jjuun0 <jh061993@gmail.com>
2021-05-19 21:33:59 +0900

    Update cam_capture.py

--- cam_capture.py
+++ cam_capture.py
@@ -1,17 +1,18 @@
-import cv2 as cv
+import cv2
 import time
 
 
 def capture(save_name):
-    camera = cv.VideoCapture(0)
+    camera = cv2.VideoCapture(0)
 
     ret, image = camera.read()
-    time.sleep(2)
-    cv.imwrite(save_name, image)
+    # time.sleep(2)
+    cv2.imwrite(save_name, image)
 
     camera.release()
-    cv.destroyAllWindows()
+    cv2.destroyAllWindows()
 
 
 if __name__ == '__main__':
-    capture()
+    print(cv2.__version__)
+    # capture()
