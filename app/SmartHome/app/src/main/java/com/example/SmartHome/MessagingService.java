79d3bf91af76c89cc5fe307f07fd6c97dad05f63
jjuun0 <jh061993@gmail.com>
2021-05-28 21:47:51 +0900

    Add FCM Notification with aws lambda
    알림 기능 추가

---
+++ app/SmartHome/app/src/main/java/com/example/SmartHome/MessagingService.java
@@ -0,0 +1,73 @@
+package com.example.SmartHome;
+
+import android.app.Notification;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Context;
+import android.content.Intent;
+import android.os.Build;
+import android.util.Log;
+import android.widget.Toast;
+
+import androidx.core.app.NotificationCompat;
+
+import com.google.firebase.messaging.FirebaseMessagingService;
+import com.google.firebase.messaging.RemoteMessage;
+
+public class MessagingService extends FirebaseMessagingService {
+    private static final String[] topics = {"/topics/notify"};  // 주제 경로 설정
+
+    @Override
+    public void onMessageReceived(RemoteMessage remoteMessage) {
+        super.onMessageReceived(remoteMessage);
+        makeNotification(remoteMessage);
+    }
+
+    private void makeNotification(RemoteMessage remoteMessage) {
+        try {
+            int notificationId = -1;
+            Context mContext = getApplicationContext();
+
+            Intent intent = new Intent(this, MainActivity.class);
+            intent.setAction(Intent.ACTION_MAIN);
+            intent.addCategory(Intent.CATEGORY_LAUNCHER);
+
+            String title = remoteMessage.getData().get("title");
+            String message = remoteMessage.getData().get("body");
+            String topic = remoteMessage.getFrom();
+
+            NotificationManager notificationManager = (NotificationManager) mContext.getSystemService(Context.NOTIFICATION_SERVICE);
+            NotificationCompat.Builder builder = new NotificationCompat.Builder(this, "10001");
+            if (Build.VERSION.SDK_INT < Build.VERSION_CODES.O) {
+                builder.setVibrate(new long[] {200, 100, 200});
+            }
+            builder.setSmallIcon(R.drawable.ic_launcher_background)
+                    .setAutoCancel(true)
+                    .setDefaults(Notification.DEFAULT_SOUND)
+                    .setContentTitle(title)
+                    .setContentText(message);
+
+            if (topic.equals(topics[0])) {
+                notificationId = 0;
+            } else if (topic.equals(topics[1])) {
+                notificationId = 1;
+            }
+
+            if (notificationId >= 0) {
+                PendingIntent pendingIntent = PendingIntent.getActivity(this, notificationId, intent, PendingIntent.FLAG_CANCEL_CURRENT);
+                builder.setContentIntent(pendingIntent);
+                notificationManager.notify(notificationId, builder.build());
+            }
+
+        } catch (NullPointerException nullException) {
+            Toast.makeText(getApplicationContext(), "알림에 오류가 발생했습니다.", Toast.LENGTH_SHORT).show();
+            Log.e("error Notify", nullException.toString());
+        }
+    }
+
+    @Override
+    public void onNewToken(String s) {
+        Log.e("token?", s);
+        super.onNewToken(s);
+    }
+}
