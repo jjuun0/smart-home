afca207583992a831f9b817084f30d52fd7709f4
jjuun0 <jh061993@gmail.com>
2021-06-25 19:06:47 +0900

    add "Process" Key -> Value : "Enroll" or "Delete"

--- Lambda/scanFingerPrint.py
+++ Lambda/scanFingerPrint.py
@@ -6,44 +6,66 @@ dynamodb = boto3.resource('dynamodb')
 
 
 def lambda_handler(event, context):
-    id, name = event['ID'], event['Name']
-
     table = dynamodb.Table('FingerPrint')
-
-    id_response = table.query(
-        KeyConditionExpression=Key('ID').eq(id)
-    )
-
-    print("id scan result: ", id_response)
-
-    name_response = table.query(
-        IndexName='Name-index',
-        KeyConditionExpression=Key('Name').eq(name)
-    )
-
-    print("name scan result: ", name_response)
-
-    # topic publish
     client = boto3.client('iot-data', region_name='ap-northeast-2')
 
-    if id_response['ScannedCount'] != 0:
-        enroll = "False"
-        message = "Already existed id"
+    process, id, name = event["Process"], event['ID'], event['Name']
 
-    elif name_response['ScannedCount'] != 0:
-        enroll = "False"
-        message = "Already existed name"
+    if process == 'Enroll':
 
-    else:
-        enroll = "True"
-        message = "Allow to enroll"
+        id_response = table.query(
+            KeyConditionExpression=Key('ID').eq(id)
+        )
+
+        print("id scan result: ", id_response)
+
+        name_response = table.query(
+            IndexName='Name-index',
+            KeyConditionExpression=Key('Name').eq(name)
+        )
+
+        print("name scan result: ", name_response)
+
+        # topic publish
+        if id_response['ScannedCount'] > 0:
+            allow = "False"
+            message = "Already existed id"
+
+        elif name_response['ScannedCount'] > 0:
+            allow = "False"
+            message = "Already existed name"
+
+        else:
+            allow = "True"
+            message = "Allow to enroll"
+
+
+
+
+    else:  # process = 'Delete'
+        db_response = table.query(
+            KeyConditionExpression=Key('ID').eq(id) & Key('Name').eq(name)
+            # FilterConditionExpression=Key('Name').gr(name)
+        )
+
+        print("Delete db response : ", db_response)
+
+        if db_response['ScannedCount'] > 0:
+            allow = "True"
+            message = "Allow to delete"
+
+        else:
+            allow = "False"
+            message = "id, name are not in DB"
 
     response = client.publish(
-        topic='aws/fingerprint/enroll',
+        topic='aws/fingerprint/db/scan',
         qos=1,
-        payload=json.dumps({"Enroll": enroll, "Message": message})
+        payload=json.dumps({"Allow": allow, "Message": message})
     )
 
     print("topic publish result: ", response)
+
+    return
 
 
